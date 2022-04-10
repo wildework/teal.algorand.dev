59fb7933c41e9b534310b0a1d4cd082ef9eccf9b
decentMorgan <morgan@decentology.com>
2022-04-11 01:34:02 +0300

    Refactored App to use all the new React components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,112 +1,111 @@
-// import {useEffect} from 'react';
+import {cx, css} from '@emotion/css';
+import {BrowserRouter, Routes, Route, Outlet, Link} from 'react-router-dom';
+
 import {Provider as AlgorandProvider, useAlgorand} from './Algorand';
 import * as Counter from './AlgorandCounter';
 
-function App() {
+import * as Pages from './Pages';
+
+const styles = {
+  container: css({
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    overflow: 'auto',
+    backgroundColor: '#e0e0e0'
+  }),
+  head: css({
+    boxSizing: 'border-box',
+    display: 'flex',
+    flexDirection: 'row',
+    padding: '32px',
+    borderBottom: '1px solid #eeeeee',
+    backgroundColor: '#f5f5f5',
+  }),
+  body: css({
+    boxSizing: 'border-box',
+    flex: '1',
+    padding: '32px',
+    backgroundColor: '#fafafa',
+  }),
+  feet: css({
+    boxSizing: 'border-box',
+    padding: '32px 32px 64px 32px',
+    borderTop: '1px solid #eeeeee',
+    backgroundColor: '#fafafa',
+  }),
+};
+
+
+
+function App(props) {
   const algorand = useAlgorand();
-  const counter = Counter.useCounter();
 
   console.log(algorand.state);
-  console.log(counter);
+
+  const onToggleAuthentication = () => {
+    if (algorand.state.account) {
+      algorand.disconnect();
+    } else {
+      algorand.connect();
+    }
+  };
 
   return (
-    <div
-      className="App"
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100%'
-      }}
-    >
-      <nav className="navbar is-transparent">
-        <div className="navbar-brand">
-          <h1 className="title is-4" style={{margin: '0'}}>
-            <a className="navbar-item" href="https://bulma.io" style={{color: 'black'}}>
-              Algorand.dev
-            </a>
-          </h1>
-          <div className="navbar-burger burger" data-target="navbarExampleTransparentExample">
-            <span></span>
-            <span></span>
-            <span></span>
+    <div className={cx(styles.container)}>
+      <div className={cx(styles.head)}>
+        <h1 className="title" style={{flex: '1', margin: '0'}}>
+          <Link to="/" style={{fontSize: '0.7em'}}>
+            Algorand.dev
+          </Link>
+        </h1>
+        <button className="button is-primary" onClick={onToggleAuthentication}>
+          <span className="icon">
+            <i className="far fa-user"></i>
+          </span>
+          <span>{algorand.state.account ? 'Disconnect' : 'Connect'}</span>
+        </button>
+      </div>
+      <div className={cx(styles.body)}>
+        {algorand.state.account &&
+          <div className="tags has-addons">
+            <span className="tag is-dark">Account</span>
+            <span className="tag" style={{justifyContent: 'flex-start', width: '200px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'}}>{algorand.state.account}</span>
           </div>
-        </div>
-
-        <div id="navbarExampleTransparentExample" className="navbar-menu">
-          <div className="navbar-start">
-            <a className="navbar-item" href="/">Home</a>
-          </div>
-
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="field is-grouped">
-                <p className="control">
-                  <a className="button is-primary" href="/">
-                    <span className="icon">
-                      <i className="far fa-envelope-open"></i>
-                    </span>
-                    <span>Contact</span>
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-
-      <section className="hero is-medium is-primary is-bold">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">Developers!!!</h1>
-            <h2 className="subtitle">Algorand is coming</h2>
-          </div>
-        </div>
-      </section>
-
-      <section className="section is-flex-grow-1">
-      
-        <div className="container">
-        
-          <h1 className="title">Sandbox</h1>
-          <h2 className="subtitle">Watch out below...</h2>
-          <p className="block">
-            <button onClick={algorand.connect}>Connect</button>
-            <button onClick={algorand.disconnect}>Disconnect</button>
-          </p>
-          {algorand.state.account && <pre className="block">{algorand.state.account}</pre>}
-          {counter &&
-            <p className="block">
-              <button onClick={counter.deploy}>Deploy</button>
-              <button onClick={counter.redeploy}>Redeploy</button>
-              <button onClick={counter.add}>Add</button>
-              <button onClick={counter.addABI}>Add ABI</button>
-            </p>
-          }
-        </div>
-      </section>
-
-      <footer className="footer">
-        <div className="content has-text-centered">
-          <p>
-            The future of <strong>Algorand</strong> adoption
-          </p>
-        </div>
-      </footer>
+        }
+        <Outlet />
+      </div>
+      <div className={cx(styles.feet)}>
+        Built by
+        {' '}
+        <a href="https://twitter.com/algorand_dev">
+          Morgan Wilde
+        </a>
+      </div>
     </div>
   );
 }
 
 function WrappedApp(props) {
   return (
-    <AlgorandProvider
-      modules={[
-        {
-          bundle: Counter
-        }
-      ]}
-    >
-      <App />
-    </AlgorandProvider>
+    <BrowserRouter>
+      <AlgorandProvider
+        modules={[
+          {
+            bundle: Counter
+          }
+        ]}
+      >
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Pages.Menu />} />
+            <Route path="/resources" element={<Pages.Resources />} />
+            <Route path="/teal" element={<Pages.TEAL />} />
+            <Route path="/counter" element={<Pages.Counter />} />
+          </Route>
+        </Routes>
+      </AlgorandProvider>
+    </BrowserRouter>
   );
 }
 
